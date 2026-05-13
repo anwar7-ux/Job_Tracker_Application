bcd7bfc8493a54ce786cd1c22a93820ce69c4b76
D Anwar Baba <annuanwar225@gmail.com>
2026-05-13 13:41:24 +0530

    Adding ForgotPassword Component route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
-import { ProtectedUserRoute, ProtectedAdminRoute, ProtectedHRRoute } from './components/ProtectedRoutes'
-import { UserLayout, AdminLayout, HRLayout } from './components/common/Layouts'
+import ForgotPassword from './pages/auth/ForgotPassword'
+
+import {
+  ProtectedUserRoute,
+  ProtectedAdminRoute,
+  ProtectedHRRoute
+} from './components/ProtectedRoutes'
+
+import {
+  UserLayout,
+  AdminLayout,
+  HRLayout
+} from './components/common/Layouts'
 
 // User pages
 import Dashboard from './pages/user/Dashboard'
@@ -28,31 +40,34 @@ import HRRegisterAdmin from './pages/hr/HRRegisterAdmin'
 function App() {
   return (
     <Routes>
+
+      {/* Public Routes */}
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
 
       {/* USER */}
       <Route element={<ProtectedUserRoute />}>
         <Route element={<UserLayout />}>
-          <Route path="/dashboard"       element={<Dashboard />} />
-          <Route path="/job-board"       element={<JobBoard />} />
-          <Route path="/post-job"        element={<PostJob />} />
-          <Route path="/my-posts"        element={<MyPosts />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/job-board" element={<JobBoard />} />
+          <Route path="/post-job" element={<PostJob />} />
+          <Route path="/my-posts" element={<MyPosts />} />
           <Route path="/my-applications" element={<MyApplications />} />
-          <Route path="/notifications"   element={<NotificationsPage />} />
-          <Route path="/profile"         element={<ProfilePage />} />
+          <Route path="/notifications" element={<NotificationsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Route>
       </Route>
 
       {/* ADMIN */}
       <Route element={<ProtectedAdminRoute />}>
         <Route element={<AdminLayout />}>
-          <Route path="/admin"                   element={<AdminDashboard />} />
-          <Route path="/admin/users"             element={<AdminUsers />} />
-          <Route path="/admin/site-header"       element={<AdminSiteHeader />} />
-          <Route path="/admin/site-footer"       element={<AdminSiteFooter />} />
-          <Route path="/admin/navigation"        element={<AdminNavigation />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/users" element={<AdminUsers />} />
+          <Route path="/admin/site-header" element={<AdminSiteHeader />} />
+          <Route path="/admin/site-footer" element={<AdminSiteFooter />} />
+          <Route path="/admin/navigation" element={<AdminNavigation />} />
           <Route path="/admin/footer-navigation" element={<AdminFooterNavigation />} />
         </Route>
       </Route>
@@ -60,12 +75,14 @@ function App() {
       {/* HR */}
       <Route element={<ProtectedHRRoute />}>
         <Route element={<HRLayout />}>
-          <Route path="/hr"                 element={<HRDashboard />} />
-          <Route path="/hr/register-admin"  element={<HRRegisterAdmin />} />
+          <Route path="/hr" element={<HRDashboard />} />
+          <Route path="/hr/register-admin" element={<HRRegisterAdmin />} />
         </Route>
       </Route>
 
+      {/* Fallback */}
       <Route path="*" element={<Navigate to="/login" replace />} />
+
     </Routes>
   )
 }
